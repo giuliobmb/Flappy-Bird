e5ef9ed0c1e5f3e932a735a602bc3381df49e2fc
giulio <74022973+giuliobmb@users.noreply.github.com>
2021-12-13 11:57:01 +0100

    bg scroll

--- main.js
+++ main.js
@@ -11,23 +11,28 @@ sprites.src = 'SPRITES.png';
 
 //ctx.drawImage(sprites, 100, 100);
 
-const background = new Sprite(sprites, 0, 0, 153, 957);
+const background = new Sprite(sprites, 0, 0, 130, 900);
 
 
-
+let dX=0
+let dY=0;
+let dW=320;
+let dH=560;
 
 function gameloop(){
     ctx.clearRect(0, 0, canvas.width, canvas.height);
-    let dX=0
-    let dY=0;
-    let dW=320;
-    let dH=560;
+    
+    dX--;
     
     background.draw(dX, dY, dW, dH);
+    background.draw(dX+dW, dY, dW, dH);
+
+    if(dX<dW*-1)
+        dX = 0;
 
     
     
-    window.requestAnimationFrame(gameloop);
+    requestAnimationFrame(gameloop);
 }
 
 gameloop();
