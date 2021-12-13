465c9812bbbe68295c6ef701dc0b9251c8a0480b
giuliobmbb <74022973+giuliobmb@users.noreply.github.com>
2021-12-13 16:06:00 +0100

    set class game

--- main.js
+++ main.js
@@ -3,29 +3,25 @@ const ctx = canvas.getContext('2d');
 
 export { ctx };
 
-import { Sprite } from './modules/classes.js';
+import { Sprite, Game } from './modules/classes.js';
 
 const sprites = new Image();
 sprites.src = 'SPRITES.png';
 
 
-//ctx.drawImage(sprites, 100, 100);
 
-const background = new Sprite(sprites, 0, 0, 130, 900);
+let game = new Game(sprites)
 
 
-let dX=0
-let dY=0;
-let dW=320;
-let dH=560;
+
+
+
+
+
 
 function gameloop(){
     ctx.clearRect(0, 0, canvas.width, canvas.height);
-    
-    background.draw(dX, dY, dW, dH);;
-
-
-    
+    game.setup();
     
     requestAnimationFrame(gameloop);
 }
--- modules/classes.js
+++ modules/classes.js
@@ -13,4 +13,24 @@ class Sprite{
     }
 }
 
-export { Sprite };
+
+class Game{
+    constructor(elements){
+        this.elements = elements;
+        this.mode;
+    }
+    setup(){
+        const background = new Sprite(this.elements, 0, 0, 130, 900);
+        background.draw(0, -6, 320, 560);
+        const terrain = new Sprite(this.elements, 150, 2, 150, 50);
+        terrain.draw(0, 118, 320, 40);
+        
+    }
+    setMode(mode){
+
+    }
+
+
+}
+
+export { Sprite, Game};
